e6c71b5205eca73681636c1e2ef4dca26574a123
Pallavi Kandikanti <pallavikandikantica@gmail.com>
2025-04-05 14:52:38 -0400

    deleted previous files

--- CumulativeAssignment_01/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CumulativeAssignment_01.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
